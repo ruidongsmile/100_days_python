b3c2c97937154ece99f37bbb5fb1519287d3f748
Rui Dong <ruidongsmile@gmail.com>
2022-06-19 23:15:46 +0200

    Day 30 learned try except statment

---
+++ day_30/exercise/main.py
@@ -0,0 +1,31 @@
+# # FileNotFound
+# #
+# try:
+#     file = open("a_file.txt")
+#     a_dict = {"key": "value"}
+#     print(a_dict["key"])
+# except FileNotFoundError:
+#     file = open("a_file.txt", "w")
+#     file.write("Something")
+# except KeyError as error_message:
+#     print(f"The key {error_message} does not exist.")
+# else:
+#     content = file.read()
+#     print(content)
+# finally:
+#     # file.close()
+#     # print("File was closed")
+#     raise TypeError("This  is an error I made up.")
+#
+
+height = float(input("Height: "))
+weight = int(input("Weight: "))
+
+if height > 3:
+    raise ValueError("Human Height should not be over 3 meters.")
+
+bmi = weight / height ** 2
+print(bmi)
+
+
+
